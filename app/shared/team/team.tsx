ee3d46d476d0b616d97aea3a67220c31c58f9dce
Owner <ghifariezraramadhan@gmail.com>
2025-05-29 09:43:10 +0700

    update page team

--- app/shared/team/team.tsx
+++ app/shared/team/team.tsx
@@ -3,7 +3,7 @@ export default function TeamSection() {
   return (
     <section className="font-[family-name:var(--font-geist-sans)] bg-white">
       <div className="flex flex-col gap-2 md:gap-5 lg:gap-10 items-center justify-center bg-cover bg-center py-32 sm:py-36 xl:py-40 px-8" style={{ backgroundImage: "url('/hero-bg.png')" }}>
-        <div className="flex flex-col items-center justify-center px-8 mb-[20px] md:mb-[40px] lg:mb-[60px] xl:mb-[80px]">
+        <div className="flex flex-col items-center justify-center mb-[20px] md:mb-[40px] lg:mb-[60px] xl:mb-[80px]">
           <div className="flex flex-col items-center justify-center w-full h-full gap-[18px]">
             <h1 className="font-semibold text-center text-[32px] sm:text-4xl md:text-5xl lg:text-6xl">Struktur Organisasi Perusahaan</h1>
             <p className="text-center text-(--color-description) text-sm sm:text-xl align-center lg:text-2xl">
