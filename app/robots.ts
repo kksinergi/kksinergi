cd7247c8b939fc57db8422d9ec667b3b1fcc58f1
Owner <ghifariezraramadhan@gmail.com>
2025-07-02 06:54:09 +0700

    UPDATE : ROBOTS

--- app/robots.ts
+++ app/robots.ts
@@ -4,20 +4,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: "Googlebot", // Izinkan Google indeks
-        allow: "/",
-      },
-      {
-        userAgent: "Bingbot", // Izinkan Bing indeks
-        allow: "/",
-      },
-      {
-        userAgent: "Applebot", // Izinkan Apple indexing (Siri & Safari)
-        allow: "/",
-      },
-      {
-        userAgent: "*", // Semua bot lain (scraper, AI crawlers, dsb.)
-        disallow: "/", // Dilarang total
+        userAgent: "*",
+        allow: "/", // Izinkan semua bot mengakses semua halaman
       },
     ],
     sitemap: "https://www.kksinergi.com/sitemap.xml",
