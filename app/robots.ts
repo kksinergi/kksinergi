4472ffe288879f2063fcab8cd859dc7155eda2dd
Owner <ghifariezraramadhan@gmail.com>
2025-07-02 07:29:52 +0700

    UPDATE : ROBOTS

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,13 @@
+// app/robots.ts
+
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: "*",
-        allow: "/", // Izinkan semua bot mengakses semua halaman
+        userAgent: "*",  // Berlaku untuk semua bot
+        allow: "/",      // Izinkan semua halaman
       },
     ],
     sitemap: "https://www.kksinergi.com/sitemap.xml",
